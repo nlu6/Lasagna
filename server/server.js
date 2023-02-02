1b0dbe4dfb3e255981d2f9f511323394c81a0cf6
Mcast77 <93240920+Mcast77@users.noreply.github.com>
2023-02-02 14:59:42 -0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-//tesmp
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'webpage')));
+
+app.listen(port, () => {
+  console.log(`server is running`)
+})
