db8c275eadced910d6624b1489565a436a436507
Mcast77 <93240920+Mcast77@users.noreply.github.com>
2023-03-24 11:13:55 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,19 @@
 // command to ssh into the server: ssh -i "LinuxKey.pem" ec2-user@ec2-54-184-67-144.us-west-2.compute.amazonaws.com
-
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express()
 const PORT = 3000;
-var id = 0;
+var id = -1;
 
-const link = mysql.createConnection({
-   host: 'localhost',
+const link = mysql.createPool({
+    connectionLimit: 5,
+    host: 'localhost',
     user: 'root',
     password: 'Group2CS386!',
     database: 'email',
  });
-
- link.connect( function(err)
- {
-  if (err) console.log(err);
-  console.log("connect");
-  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,23 +29,29 @@ app.post('/enterData', (req, res) =>
   let returnNumber = req.body.returnNumber;
   let message = req.body.messageString;
 
+  link.getConnection( (err, connection) =>
+  {
+   if (err) console.log(err);
+   console.log("connect");
+
   let command = 'INSERT INTO email (??,??,??,??) VALUES (?,?,?,?)';
   let commandString = mysql.format(command, ["sendNumber", "numberTwo", "message","id",
   sendNumbers, returnNumber, message,id]);
 
-  link.query(commandString, (err, res)=>
+  connection.query(commandString, (err, res)=>
   {
     if (err) console.log(err);
     console.log("Data added");
   });
 
-  link.query("select * from email;", (err, rows) =>
+  connection.query("select * from email;", (err, rows) =>
   {
    if (err) console.log(err);
    console.log("data: ", rows);
-   res.send("testing");
+   connection.release();
   });
-
+  console.log("Connection released");
+  });
 });
 
 app.use(express.static(path.join(__dirname,'webpage')));
@@ -59,4 +59,3 @@ app.use(express.static(path.join(__dirname,'webpage')));
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
 });
-
