ee038f7412d480e06e20e706a242bcd5bdc37c5d
DFrancs <32278209+DFrancs@users.noreply.github.com>
2023-03-26 20:56:58 -0700

    Added pytest file for main_module

---
+++ server/main_module_test.py
@@ -0,0 +1,18 @@
+import main_module
+
+test = main_module
+
+def test_getMessage():
+	assert test.getMessage() == ''
+
+def test_setMessage():
+	assert test.setMessage('Test test 1234') == 'Test test 1234'
+
+def test_getReceiving():
+	assert test.getReceiving() == ''
+
+def test_setReceiving():
+	assert test.setReceiving('6692248986, 8082698655') == ["6692248986@txt.att.net", "8082698655@vtext.com"]
+
+def test_getCarrier():
+	assert test._getCarrier('8082698655') == "Verizon Wireless"
