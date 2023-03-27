d5cec1f303176b878501038e8900751a09b406ac
Mcast77 <93240920+Mcast77@users.noreply.github.com>
2023-03-26 21:49:40 -0700

    Update server.js
    removed extra code, and fixed where python is getting called from

--- server/server.js
+++ server/server.js
@@ -77,7 +77,7 @@ app.post('/enterData', (req, res) =>
    connection.release();
   });
   let spawn = require("child_process").spawn;
-  let pythonScript = spawn('python', ["STMPRelay.py", sendNumbers, returnNumber, message]);
+  let pythonScript = spawn('python', ["main_module.py", sendNumbers, returnNumber, message]);
   pythonScript.stdout.on('data', function(data) {
   console.log(data.toString());});
   console.log("Connection released");
@@ -90,40 +90,3 @@ app.use(express.static(path.join(__dirname,'webpage')));
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
 });
-
-// function numberOfSendNumbers( senderString )
-//    {
-//     let index;
-//     let n = 0;
-
-//     for(index = 0; index < senderString.length; index++)
-//        {
-//         if( senderString.charAt(index) == ',' )
-//            {
-//             n++;
-//            }
-//        }
-    
-//     return n + 1;
-//    }
-
-// function getMessage( objectString )
-// {
-//   var returnString = objectString.replace("[{\"message\":\"","");
-//   returnString = returnString.replace("\"}]", "");
-//   return returnString;
-// }
-
-// function getTargetNumbers( objectString )
-// {
-//   var returnString = objectString.replace("[{\"sendNumber\":\"","");
-//   returnString = returnString.replace("\"}]", "");
-//   return returnString;
-// }
-
-// function getReciverNumbers( objectString )
-// {
-//   var returnString = objectString.replace("[{\"numberTwo\":\"","");
-//   returnString = returnString.replace("\"}]", "");
-//   return returnString;
-// }
